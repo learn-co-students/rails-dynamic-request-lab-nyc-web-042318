95bbd3efbe562278ba6de92acdb8a9a108e221b3
Canino Developments <ian.canino@gmail.com>
2018-05-18 15:09:32 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,11 @@
 class StudentsController < ApplicationController
   def index
     @students = Student.all
+# reender is not necessary because it is implicit by filename
+  end
+
+  def show
+    @student = Student.find(params[:id])
+# reender is not necessary because it is implicit by filename
   end
 end
